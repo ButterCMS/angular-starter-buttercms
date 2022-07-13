a685610731884d64e5076ee7970b792c9dce614d
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-07-13 10:58:55 +0200

    feat(config): script for building config using dotenv

---
+++ scripts/setenv.ts
@@ -0,0 +1,36 @@
+const { writeFile } = require('fs');
+const { config } = require('dotenv');
+const yargs = require('yargs');
+
+config();
+
+let argv = yargs
+  .option('environment', {
+    alias: 'env',
+    choices: ['prod', 'dev'],
+    demandOption: true,
+  }).parseSync();
+
+console.log(argv)
+
+const isProduction = argv.environment === 'prod';
+
+const targetPath = isProduction
+   ? `./src/environments/environment.prod.ts`
+   : `./src/environments/environment.ts`;
+
+
+const environmentFileContent = `
+export const environment = {
+   production: ${isProduction},
+   NG_APP_ANGULAR_BUTTER_CMS_API_KEY: "${process.env.NG_APP_ANGULAR_BUTTER_CMS_API_KEY}",
+   NG_APP_ANGULAR_BUTTER_CMS_PREVIEW: "${process.env.NG_APP_ANGULAR_BUTTER_CMS_PREVIEW}"
+};
+`;
+
+writeFile(targetPath, environmentFileContent, function (err) {
+   if (err) {
+      console.log(err);
+   }
+   console.log(`Wrote variables to ${targetPath}`);
+});
