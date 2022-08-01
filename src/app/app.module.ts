c20c9ad75d21ff76fd6fe194302f75165fd2c58d
Karolína <karolina.pikorova@outlook.com>
2022-08-01 19:06:29 +0200

    Routes are all missing trailing slashes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,38 @@
 import { NgModule } from '@angular/core';
 import { COMPONENTS, IMPORTS, SERVICES } from '.';
 import { AppComponent } from './app.component';
+import {Location, PathLocationStrategy} from '@angular/common';
+
+const _orig_prepareExternalUrl = PathLocationStrategy.prototype.prepareExternalUrl;
+
+PathLocationStrategy.prototype.prepareExternalUrl = function(internal) {
+  const url = _orig_prepareExternalUrl.call(this, internal);
+
+  if (url === '') {
+    return url;
+  }
+  if (url.endsWith('.html')) {
+    return url;
+  }
+  if (url.endsWith('/')) {
+    return url;
+  }
+  return url + '/';
+
+};
+
+Location.stripTrailingSlash = function (url) {
+  const /** @type {?} */ match = url.match(/#|\?|$/);
+  const /** @type {?} */ pathEndIdx = match && match.index || url.length;
+  const /** @type {?} */ droppedSlashIdx = pathEndIdx - (url[pathEndIdx - 1] === '/' ? 1 : 0);
+  const first = url.slice(0, droppedSlashIdx);
+  const last = url.slice(pathEndIdx);
+
+  if (first.endsWith('.html')) {
+    return first + last;
+  }
+  return first + '/' + last;
+};
 
 @NgModule({
   declarations: [
